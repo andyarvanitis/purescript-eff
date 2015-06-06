8c0233eb65745d80dd3b0f27dea5a18267a39070
Andy Arvanitis <andyarvanitis@gmail.com>
2015-06-06 09:51:04 -0700

    Implement remaining C++ FFI

--- src/Control/Monad/Eff.hh
+++ src/Control/Monad/Eff.hh
@@ -20,6 +20,13 @@
 namespace Control_Monad_Eff {
   using namespace PureScript;
 
+  template <typename A>
+  inline auto returnE(A a) -> eff_fn<A> {
+    return [=]() {
+      return a;
+    };
+  }
+
   template <typename A, typename B>
   inline auto bindE(eff_fn<A> a) -> fn<fn<A,eff_fn<B>>,eff_fn<B>> {
     return [=](fn<A,eff_fn<B>> f) {
@@ -30,9 +37,51 @@ namespace Control_Monad_Eff {
   }
 
   template <typename A>
-  inline auto returnE(A a) -> eff_fn<A> {
+  inline auto runPure(eff_fn<A> f) -> A {
+    return f();
+  }
+
+  template <typename A>
+  inline auto untilE(eff_fn<bool> f) -> Prelude::Unit {
     return [=]() {
-      return a;
+      while (!f());
+      return Prelude::unit;
+    };
+  }
+
+  template <typename A>
+  inline auto whileE(eff_fn<bool> f) -> fn<eff_fn<A>,Prelude::Unit> {
+    return [=](eff_fn<A> a) {
+      return [=]() {
+        while (f()) {
+          a();
+        }
+        return Prelude::unit;
+      };
+    };
+  }
+
+  template <typename A>
+  inline auto forE(double lo) {
+    return [=](double hi) {
+      return [=](fn<double,eff_fn<Prelude::Unit>> f) {
+        return [=]() {
+          for (auto i = lo; i < hi; i++) {
+            f(i)();
+          }
+        };
+      };
+    };
+  }
+
+  template <typename A>
+  inline auto foreachE(list<A> as) {
+    return [=](fn<A,eff_fn<Prelude::Unit>> f) {
+      return [=]() {
+        for (auto it = as.begin(); it != as.end(); ++it) {
+          f(as(*it))();
+        }
+      };
     };
   }
 }
